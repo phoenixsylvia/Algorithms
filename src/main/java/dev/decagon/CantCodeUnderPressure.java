592effb5c201febceb60ea4a77ecb11a00cf9ac8
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 12:21:20 +0100

    "A function that doubles the integer and returns it".

---
+++ src/main/java/dev/decagon/CantCodeUnderPressure.java
@@ -0,0 +1,10 @@
+package dev.decagon;
+
+public class CantCodeUnderPressure {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        int result = 0;
+        result = i * 2;
+        return result;
+    }
+}
