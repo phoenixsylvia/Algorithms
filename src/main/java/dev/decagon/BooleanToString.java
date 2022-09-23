dde6cab29a3fad48d6cf67b93f06ade499961483
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 12:36:36 +0100

    "A method that takes a boolean value and return a "Yes" string for true, or a "No" string for false."

---
+++ src/main/java/dev/decagon/BooleanToString.java
@@ -0,0 +1,10 @@
+package dev.decagon;
+
+public class BooleanToString {
+    public static String boolToWord(boolean b)
+    {
+        String result = "";
+        return result =  b ? "Yes": "No";
+    }
+
+}
