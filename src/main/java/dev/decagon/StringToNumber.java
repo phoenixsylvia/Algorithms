d47a0d8470541d8e5ef9c183680e436bb0704392
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 10:12:45 +0100

    Algo update

---
+++ src/main/java/dev/decagon/StringToNumber.java
@@ -0,0 +1,10 @@
+package dev.decagon;
+
+public class StringToNumber {
+        public static int stringToNumber(String str) {
+            //TODO: Convert str into a number
+
+            return Integer.parseInt(str);
+
+        }
+    }
