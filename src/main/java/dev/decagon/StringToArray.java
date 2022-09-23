05f59763ba5dc2293fbdc244a0e140dc588f76b6
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 12:41:53 +0100

    "A function to split a string and convert it into an array of words."

---
+++ src/main/java/dev/decagon/StringToArray.java
@@ -0,0 +1,10 @@
+package dev.decagon;
+
+public class StringToArray {
+    public static String[] stringToArray(String s) {
+        //your code;
+        String [] newArr = s.split(" ");
+        // System.out.println(newArr);
+        return newArr;
+    }
+}
