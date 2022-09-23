1e75cdea91d243d85d9c3500b8299721d6ea00da
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 11:39:07 +0100

    "A program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0."

---
+++ src/main/java/dev/decagon/GrassHopper.java
@@ -0,0 +1,15 @@
+package dev.decagon;
+
+    public class GrassHopper {
+
+        public static int summation(int n) {
+
+            int result = 0;
+
+            for(int i= 1; i <= n; i++){
+                result += i;
+            }
+            return result;
+        }
+    }
+
