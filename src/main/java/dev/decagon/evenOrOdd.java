3241313e62bff38f8c89748de30cd0be18fcf044
phoenixsylvia <sylviananev@gmail.com>
2022-09-23 11:30:58 +0100

    "A function to check that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. "

---
+++ src/main/java/dev/decagon/evenOrOdd.java
@@ -0,0 +1,14 @@
+package dev.decagon;
+
+public class evenOrOdd {
+
+        public static String even_or_odd(int number) {
+            //Place code here
+            if(number % 2 == 0){
+                return "Even";
+            } else{
+                return "Odd";
+            }
+        }
+    }
+
