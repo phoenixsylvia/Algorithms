9a4d1532f4f2f43615e05956c0f05e7f963c1ed7
phoenixsylvia <sylviananev@gmail.com>
2022-11-15 21:53:59 +0100

    "add backslash test"

---
+++ src/main/java/dev/decagon/BackSlashTest.java
@@ -0,0 +1,12 @@
+package dev.decagon;
+
+public class BackSlashTest {
+    public static void main(String[] args) {
+        System.out.println("\\/");
+        System.out.println("\\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println("//\\\\");
+        System.out.println("/\\");
+    }
+}
